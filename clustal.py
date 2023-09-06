7458c29774b199ff3e7bb25e84e7ac839d20fbe8
Cassandra GBABOUA <cassandragbaboua@eduroam-prg-cd-1-10-152.net.univ-paris-diderot.fr>
2023-09-06 10:16:21 +0200

    modification programme clustal

--- clustal.py
+++ clustal.py
@@ -2,11 +2,13 @@ def matrix(matrix_file):
 	blosum62 = {}
 	aa = []
 	with open (matrix_file,'r') as file :
-		aa = file.readlines().strip().split()
+		first_line = file.readline().strip()
+		aa = first_line.split()
 		for line in file :
 			row_values = list(map(int, line.strip().split()[1:]))
-        	row_acid = line.strip().split()[0]
-        	for col_acid, value in zip(amino_acids, row_values):
-            	blosum62[(row_acid, col_acid)] = value
+			row_acid = line.split()[0]
+			for col_acid, value in zip(aa, row_values):
+				blosum62[(row_acid, col_acid)] = value
+	return blosum62
 
 
